04431697431be654ac3571b4c7b28692ff02484d
RobertMN076 <robert.giulio@escolar.ifrn.edu.br>
2024-10-22 09:56:34 -0300

    att

--- teste.ts
+++ teste.ts
@@ -1,7 +1,7 @@
 class ContaCorrente {
     numero:number;
     titular:string;
-    saldo:number;
+    private saldo:number;
 
     constructor(aNumero:number, aTitular:string, aSaldo:number){
         this.numero = aNumero;
@@ -14,14 +14,29 @@ class ContaCorrente {
     }
 
     sacar(valor:number) {
-        this.saldo -= valor;
+        if (valor > 0) {
+            if(this.saldo >= valor) {
+                this.saldo -= valor
+            } else {
+                throw new Error ('Saldo insuficiente')
+            }
+        } else {
+            throw "O valor é inválido para saque"
+        }
     }
 
     exibirSaldo() {
-        console.log(this.saldo);
+        console.log("R$ " + this.saldo);
     }
 }
 
-const conta = new ContaCorrente(1, 'Marcelo', 100);
-console.log('Numero')
+
+const conta = new ContaCorrente(1, 'Robert', 100);
+console.log('Numero: ' + conta.numero);
+console.log('Titular: ' + conta.titular);
 conta.exibirSaldo()
+conta.depositar(150)
+conta.exibirSaldo()
+conta.sacar(100)
+conta.exibirSaldo()
+conta.sacar(-200)
